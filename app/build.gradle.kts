1cddb09aabb5a30c4c41a7a9dffad8aeee5f6080
Kiru-Sama <kiru-sama@users.noreply.github.com>
2026-08-23 18:46:35 +0800

    chore: 添加 Rikka+ Debug APK 构建 workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
     compileSdk = 37
 
     defaultConfig {
-        applicationId = "me.rerere.rikkahub"
+        applicationId = "com.kirusi.rikkahub"
         minSdk = 26
         targetSdk = 37
         versionCode = 178
@@ -79,7 +79,7 @@ android {
             buildConfigField("String", "VERSION_CODE", "\"${android.defaultConfig.versionCode}\"")
         }
         debug {
-            applicationIdSuffix = ".debug"
+            // applicationIdSuffix = ".debug"
             buildConfigField("String", "VERSION_NAME", "\"${android.defaultConfig.versionName}\"")
             buildConfigField("String", "VERSION_CODE", "\"${android.defaultConfig.versionCode}\"")
         }
